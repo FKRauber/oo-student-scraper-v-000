1347af81a0b29f4236b33b29b7ca97bd1917fd47
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-05-19 23:31:31 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,8 +21,15 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(open(profile_url))
     student = {}
-
+    profile_url.css()
     student
   end
+
+  #   :twitter=>"http://twitter.com/flatironschool",
+  #   :linkedin=>"https://www.linkedin.com/in/flatironschool",
+  #   :github=>"https://github.com/learn-co,
+  #   :blog=>"http://flatironschool.com",
+  #   :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+  #   :bio=> "I'm a school"
 
 end
