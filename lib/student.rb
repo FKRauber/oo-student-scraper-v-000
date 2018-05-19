205f63c12b829a5cbe650d413c1de8a4799b4f9a
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-05-19 23:47:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,11 +12,15 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    students_array.each { |k, v|
 
+    }
   end
 
   def add_student_attributes(attributes_hash)
+    attributes_hash.each { |k, v|
 
+    }
   end
 
   def self.all
